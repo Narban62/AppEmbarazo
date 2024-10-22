b061d0077d0bed4e5886385d312c89a3e1cd8aea
JosuJosue <josuensd@gmail.com>
2024-10-21 22:57:40 -0500

    Test de videos

--- lib/src/pages/modulos_page.dart
+++ lib/src/pages/modulos_page.dart
@@ -1,69 +1,62 @@
+import 'package:app_embarazo/src/pages/home_page.dart';
+import 'package:app_embarazo/src/widgets/button_widget.dart';
+import 'package:app_embarazo/src/widgets/header_widget.dart';
+import 'package:app_embarazo/src/widgets/image_widget.dart';
+import 'package:app_embarazo/src/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
-import '../widgets/background_widget.dart';
-import '../widgets/bubble_widget.dart';
-import '../widgets/button_widget.dart';
-import '../widgets/header_widget.dart';
-import '../widgets/text_widget.dart';
 
-
-class ModulosPage extends StatelessWidget {
-  const ModulosPage({Key? key}) : super(key: key);
+class ModulosPage extends StatefulWidget {
+  const ModulosPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    const Color _color1 = Color(0xffFCDEE7);
+  State<ModulosPage> createState() => _ModulosPageState();
+}
 
+class _ModulosPageState extends State<ModulosPage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      backgroundColor: const Color(0xffFCDEE7),
+      body: Column(
         children: [
-          const AnimatedBackground(color: _color1), // Fondo animado
-          const AnimatedBubbles(), // Burbujas animadas
-          Column(
-            children: [
-              const HeaderWidget(
-                color: Color(0xffF75B89),
-                text: 'Módulos',
-                isSubtitle: false,
-                showButton: false,
-              ),
-              const TextWidget(text: "Juntas hacia tu autocuidado"),
-              Button(
-                buttonName: "calidad de vida",
-                buttonColor: const Color(0xffF75B89),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/calidad_vida');
-                },
-              ),
-              Button(
-                buttonName: "proyecto de vida",
-                buttonColor: const Color(0xffF75B89),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/proyecto_vida');
-                },
-              ),
-              Button(
-                buttonName: "dieta",
-                buttonColor: const Color(0xffF75B89),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/dieta');
-                },
-              ),
-              Button(
-                buttonName: "actividad física",
-                buttonColor: const Color(0xffF75B89),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/actividad_fisica');
-                },
-              ),
-              Button(
-                buttonName: "psicoprofilaxis",
-                buttonColor: const Color(0xffF75B89),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/psicoprofilaxis');
-                },
-              ),
-            ],
+          const HeaderWidget(
+            color: Color(0xffF75B89),
+            text: 'Módulos',
+            isSubtitle: false,
+            showButton: false,
           ),
+          
+          const TextWidget(text: "Juntas hacia tu autocuidado"),
+          Button(
+              buttonName: "calidad de vida",
+              buttonColor: const Color(0xffF75B89),
+              onPressed: () {
+                Navigator.pushNamed(context, '/calidad_vida');
+              }),
+          Button(
+              buttonName: "proyecto de vida",
+              buttonColor: const Color(0xffF75B89),
+              onPressed: () {
+                Navigator.pushNamed(context, '/proyecto_vida');
+              }),
+          Button(
+              buttonName: "dieta",
+              buttonColor: const Color(0xffF75B89),
+              onPressed: () {
+                Navigator.pushNamed(context, '/dieta');
+              }),
+          Button(
+              buttonName: "actividad física",
+              buttonColor: const Color(0xffF75B89),
+              onPressed: () {
+                Navigator.pushNamed(context, '/actividad_fisica');
+              }),
+          Button(
+              buttonName: "psicoprofilaxis",
+              buttonColor: const Color(0xffF75B89),
+              onPressed: () {
+                Navigator.pushNamed(context, '/psicoprofilaxis');
+              }),
         ],
       ),
     );
--- lib/src/widgets/video_widget.dart
+++ lib/src/widgets/video_widget.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
-import 'package:app_embarazo/src/pages/full_screen_page.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -9,17 +6,8 @@ import 'package:video_player/video_player.dart';
 class VideoWidget extends StatefulWidget {
   final String videoUrl;
   final String videoImage;
-  final double width;
-  final double height;
-  final Color bgColor;
 
-  VideoWidget({
-    required this.videoUrl,
-    required this.videoImage,
-    required this.bgColor,
-    this.width = 1,
-    this.height = .8,
-  });
+  VideoWidget({required this.videoUrl, required this.videoImage});
 
   @override
   State<VideoWidget> createState() => _VideoWidgetState();
@@ -32,109 +20,77 @@ class _VideoWidgetState extends State<VideoWidget> {
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
     _loadVideo();
   }
 
-  Future<void> _loadVideo() async {
+  Future _loadVideo() async {
     final videoUrl =
-    await FirebaseStorage.instance.ref(widget.videoUrl).getDownloadURL();
+        await FirebaseStorage.instance.ref(widget.videoUrl).getDownloadURL();
 
-    // Ruta para almacenar el video en caché
-    final Directory tempDir = await getTemporaryDirectory();
-    final String videoFilePath = '${tempDir.path}/${widget.videoUrl.split('/').last}';
-
-    // Comprobar si el video ya está almacenado en caché
-    if (File(videoFilePath).existsSync()) {
-      _controller = VideoPlayerController.file(File(videoFilePath))
-        ..initialize().then((_) {
-          setState(() {
-            _isVideoReady = true;
-          });
+    _controller = VideoPlayerController.network(videoUrl)
+      ..initialize().then((_) {
+        setState(() {
+          _isVideoReady = true;
         });
-    } else {
-      // Si no existe, descargar el video
-      final response = await HttpClient().getUrl(Uri.parse(videoUrl));
-      final bytes = await response.close().then((response) => response.fold<List<int>>([], (a, b) => a..addAll(b)));
-      final videoFile = File(videoFilePath);
-      await videoFile.writeAsBytes(bytes);
-
-      // Inicializar el controlador con el archivo guardado
-      _controller = VideoPlayerController.file(videoFile)
-        ..initialize().then((_) {
-          setState(() {
-            _isVideoReady = true;
-          });
-        });
-    }
+      });
   }
 
   @override
   void dispose() {
+    // TODO: implement dispose
     _controller.dispose();
     super.dispose();
   }
 
-  void _openFullscreenVideo(BuildContext context, String videoUrl) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => FullscreenVideoPage(videoUrl: videoUrl),
-      ),
-    );
+  void _togglePlayPause() {
+    if (_isPlaying) {
+      _controller.pause();
+    } else {
+      _controller.play();
+    }
+
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-
-    return Container(
-      width: widget.width * screenWidth,
-      height: widget.height * screenWidth,
-      color: widget.bgColor,
-      child: _isVideoReady
-          ? GestureDetector(
-        onTap: () async {
-          final videoUrl = await FirebaseStorage.instance.ref(widget.videoUrl).getDownloadURL();
-          _openFullscreenVideo(context, videoUrl);
-        },
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
+    return _isVideoReady
+        ? GestureDetector(
+            onTap: _togglePlayPause,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller)),
+                if (!_isPlaying)
+                  Icon(Icons.play_arrow, size: 100, color: Colors.white)
+              ],
             ),
-            if (!_isPlaying)
-              Icon(Icons.play_arrow, size: 100, color: Colors.white),
-          ],
-        ),
-      )
-          : GestureDetector(
-        onTap: () async {
-          // Obtener la URL del video y abrir en pantalla completa
-          final videoUrl = await FirebaseStorage.instance.ref(widget.videoImage).getDownloadURL();
-          _openFullscreenVideo(context, videoUrl);
-        },
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // Mostrar la imagen del thumbnail mientras se carga el video
-            CachedNetworkImage(
-              imageUrl: widget.videoImage,
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => Icon(Icons.error),
+          )
+        : GestureDetector(
+            onTap: _togglePlayPause,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                // Mostrar la imagen del thumbnail mientras se carga el video
+                CachedNetworkImage(
+                  imageUrl: widget.videoImage,
+                  placeholder: (context, url) => CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+                // Mostrar el botón de play sobre la imagen del thumbnail
+                Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                  size: 64.0,
+                ),
+              ],
             ),
-            // Mostrar el botón de play sobre la imagen del thumbnail
-            Icon(
-              Icons.play_arrow,
-              color: Colors.white,
-              size: 64.0,
-            ),
-          ],
-        ),
-      ),
-      padding: EdgeInsets.all(10.0),
-    );
+          );
   }
 }
