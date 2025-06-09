5593bb0bf822ed7e7a45c8708380b985ab9bd9d7
Narban62 <Narban62@users.noreply.github.com>
2025-06-09 08:49:35 +0000

    deploy: 66296fe0c120ae3f9f427d185639cce25b101f0f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -207,7 +207,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
 "canvaskit/chromium/canvaskit.js.symbols": "a012ed99ccba193cf96bb2643003f6fc",
-"flutter_bootstrap.js": "2e9a0d737e9ae3a772926bee3a1fc71c",
+"flutter_bootstrap.js": "69949a7eef40818bfd68672de044485d",
 "splash/img/light-3x.png": "d2ccc55ab6ff93b4071b6e92d16bcd70",
 "splash/img/dark-4x.png": "6d1162e83021104f63123648ffd8e287",
 "splash/img/light-1x.png": "63d3f732a6666850cd5935eeaa4793db",
